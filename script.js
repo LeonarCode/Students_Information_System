eb54f2c11b415bedc83b2dfbea8ad63dd86c089c
Leonard304 <167664878+Leonard304@users.noreply.github.com>
2025-03-29 15:38:17 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function showEnroll(){
+    document.querySelector(".enrollment").classList.toggle("show");
+}
+
+function showGrade(){
+    document.querySelector(".grades").classList.toggle("show");
+}
+function showBill(){
+    document.querySelector(".billing").classList.toggle("show");
+}
